a3f4f92dcab92b7dd1c0ac52e913ac3032d38ee6
NicolasRuiz99 <nicoruiz72@gmail.com>
2024-08-04 12:55:04 -0300

    fix: changed getenv to environ.get

--- backend/config.py
+++ backend/config.py
@@ -1,4 +1,4 @@
-from os import getenv
+from os import getenv,environ
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -8,14 +8,14 @@ load_dotenv()
 SHEET_NAME = "test_movieproject"
 
 CREDENTIALS = {
-    "type": getenv("TYPE"),
-    "project_id": getenv("PROJECT_ID"),
-    "private_key_id": getenv("PRIVATE_KEY_ID"),
-    "private_key": getenv("PRIVATE_KEY"),
-    "client_email": getenv("CLIENT_EMAIL"),
-    "client_id": getenv("CLIENT_ID"),
-    "auth_uri": getenv("AUTH_URI"),
-    "token_uri": getenv("TOKEN_URI"),
-    "auth_provider_x509_cert_url": getenv("AUTH_PROVIDER_X509_CERT_URL"),
-    "client_x509_cert_url": getenv("CLIENT_X509_CERT_URL"),
+    "type": environ.get("TYPE"),
+    "project_id": environ.get("PROJECT_ID"),
+    "private_key_id": environ.get("PRIVATE_KEY_ID"),
+    "private_key": environ.get("PRIVATE_KEY"),
+    "client_email": environ.get("CLIENT_EMAIL"),
+    "client_id": environ.get("CLIENT_ID"),
+    "auth_uri": environ.get("AUTH_URI"),
+    "token_uri": environ.get("TOKEN_URI"),
+    "auth_provider_x509_cert_url": environ.get("AUTH_PROVIDER_X509_CERT_URL"),
+    "client_x509_cert_url": environ.get("CLIENT_X509_CERT_URL"),
 }
