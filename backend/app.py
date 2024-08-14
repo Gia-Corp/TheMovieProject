164fb5ac933792a76eae1b546cffad7c207f133c
Santiago Ruiz Mazzocato <santimazzo98@gmail.com>
2024-08-14 15:55:50 -0300

    chore: added pytest and ruff dependencies for backend

--- backend/app.py
+++ backend/app.py
@@ -1,52 +1,55 @@
-from flask import Flask,render_template, jsonify, request, redirect, url_for, send_file
+from flask import Flask, jsonify, request
 from google_sheet_connector import GoogleSheetConnector
 from sheets import update_movie, add_movie
 from flask_cors import CORS
 import config
-import json
-import os
 
 app = Flask(__name__)
 CORS(app)
 
+
 @app.route("/")
 def hello_world():
     return jsonify("Hello World!")
+
 
 @app.route("/movies")
 def get_movies():
     try:
         movies = GoogleSheetConnector().get_movies(5)
         return jsonify(movies)
-    except(Exception) as err:
+    except Exception as err:
         return str(err), 500
 
-@app.route("/update", methods=['POST'])
+
+@app.route("/update", methods=["POST"])
 def update():
     try:
-        id = request.json ["id"]
-        title = request.json ["title"]
-        director = request.json ["director"]
-        watched = request.json ["watched"]
-        
-        update_movie(id,title,director,watched)
-        
-        return jsonify('Success')
-    except(Exception) as err:
+        id = request.json["id"]
+        title = request.json["title"]
+        director = request.json["director"]
+        watched = request.json["watched"]
+
+        update_movie(id, title, director, watched)
+
+        return jsonify("Success")
+    except Exception as err:
         return str(err), 500
-    
-@app.route("/add", methods=['POST'])
+
+
+@app.route("/add", methods=["POST"])
 def add():
     try:
         title = request.json["title"]
         director = request.json["director"]
         watched = request.json["watched"]
-        
-        add_movie(title,director,watched)
-        
-        return jsonify('Success')
-    except(Exception) as err:
+
+        add_movie(title, director, watched)
+
+        return jsonify("Success")
+    except Exception as err:
         return str(err), 500
 
-if __name__ == '__main__':
-    app.run(host='backend', port=config.PORT, debug=True)
+
+if __name__ == "__main__":
+    app.run(host="backend", port=config.PORT, debug=True)
--- backend/sheets.py
+++ backend/sheets.py
@@ -1,5 +1,4 @@
 import config
-from os import getenv
 import gspread
 
 # print(f"koyeb app name = {config.KOYEB_APP_NAME}", flush=True)
@@ -10,33 +9,35 @@ import gspread
 gc = gspread.service_account_from_dict(config.SHEET_CREDENTIALS)
 sheet = gc.open(config.SHEET_NAME).sheet1
 
+
 def update_movie(id, title, director, watched):
     try:
         title_cell = id
         sheet.update(title_cell, title)
 
-        director_cell = f'B{title_cell[1:]}'
+        director_cell = f"B{title_cell[1:]}"
         sheet.update(director_cell, director)
 
-        watched_cell = f'C{title_cell[1:]}'
+        watched_cell = f"C{title_cell[1:]}"
         sheet.update(watched_cell, watched)
 
         return director_cell
-    except (Exception) as err:
+    except Exception as err:
         return err
+
 
 def add_movie(title, director, watched):
     try:
         row = next_available_row(sheet)
 
-        title_cell = f'A{row}'
+        title_cell = f"A{row}"
         sheet.update(title_cell, title)
 
-        director_cell = f'B{row}'
+        director_cell = f"B{row}"
         sheet.update(director_cell, director)
 
-        watched_cell = f'C{row}'
+        watched_cell = f"C{row}"
         sheet.update(watched_cell, watched)
 
-    except (Exception) as err:
+    except Exception as err:
         return err
