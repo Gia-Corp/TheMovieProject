b5d2166904c63a5bdd41fbad06959ec600443ff3
Santiago Ruiz Mazzocato <santimazzo98@gmail.com>
2024-08-19 11:18:02 -0300

    fix: added support for the release year field for movies in the list

--- backend/google_sheet_connector.py
+++ backend/google_sheet_connector.py
@@ -9,7 +9,7 @@ class GoogleSheetConnector:
 
     def get_movies(self, limit):
         raw_movies = self.sheet.get_all_records(
-            expected_headers=["Película", "Director", "Vista"]
+            expected_headers=["Película", "Director", "Vista", "Año"]
         )[0 : limit + 1]
         movies = list(map(self.transform_into_movie, raw_movies))
         return movies
@@ -19,5 +19,6 @@ class GoogleSheetConnector:
         return {
             "title": raw_movie["Película"],
             "director": raw_movie["Director"],
+            "year": raw_movie["Año"],
             "watched": watched,
         }
