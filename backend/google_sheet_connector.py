84369689d189b9981286e4d210dd1c706cbb6c3a
Santiago Ruiz Mazzocato <santimazzo98@gmail.com>
2024-08-25 21:25:30 -0300

    refactor: sheet connector now uses another api method to fetch sheet data

--- backend/google_sheet_connector.py
+++ backend/google_sheet_connector.py
@@ -1,19 +1,20 @@
+from gspread import utils
+
+
 class GoogleSheetConnector:
     def __init__(self, sheet):
         self.sheet = sheet
 
-    def get_movies(self, limit):
-        raw_movies = self.sheet.get_all_records(
-            expected_headers=["Película", "Director", "Vista", "Año"]
-        )[0 : limit + 1]
+    def get_movies_by_page(self, page_number, page_size):
+        page_first_row = 4
+        page_last_row = 5
+        raw_movies = self.sheet.get(f"A{page_first_row}:D{page_last_row }")
+        raw_movies = utils.to_records(
+            ["director", "title", "year", "watched"], raw_movies
+        )
         movies = list(map(self.transform_into_movie, raw_movies))
         return movies
 
     def transform_into_movie(self, raw_movie):
-        watched = True if raw_movie["Vista"] == "TRUE" else False
-        return {
-            "title": raw_movie["Película"],
-            "director": raw_movie["Director"],
-            "year": raw_movie["Año"],
-            "watched": watched,
-        }
+        raw_movie["watched"] = True if raw_movie["watched"] == "TRUE" else False
+        return raw_movie
