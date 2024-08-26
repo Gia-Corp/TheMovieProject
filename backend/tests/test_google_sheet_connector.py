19a42aea1b00bd67b9288ca699946615788a6f4b
Santiago Ruiz Mazzocato <santimazzo98@gmail.com>
2024-08-25 21:29:19 -0300

    test: checked cell range used in google sheet api call

--- backend/tests/test_google_sheet_connector.py
+++ backend/tests/test_google_sheet_connector.py
@@ -3,21 +3,11 @@ from unittest.mock import Mock
 
 
 class TestGoogleSheetConnector:
-    def test_when_exists_many_movies_as_get_movie_limit(self):
+    def test_connector_to_transform_sheet_data_format_in_intended_way(self):
         sheet = Mock()
-        sheet.get_all_records.return_value = [
-            {
-                "Película": "Titanic",
-                "Director": "James Cameron",
-                "Vista": "TRUE",
-                "Año": 1998,
-            },
-            {
-                "Película": "Cars",
-                "Director": "John Lasseter",
-                "Vista": "FALSE",
-                "Año": 2006,
-            },
+        sheet.get.return_value = [
+            ["James Cameron", "Titanic", 1998, "TRUE"],
+            ["John Lasseter", "Cars", 2006, "FALSE"],
         ]
         expected_movies = [
             {
@@ -34,5 +24,15 @@ class TestGoogleSheetConnector:
             },
         ]
         connector = GoogleSheetConnector(sheet)
-        movies = connector.get_movies(2)
+        movies = connector.get_movies_by_page(1, 2)
         assert movies == expected_movies
+
+    def test_connector_to_fetch_correct_sheet_range_based_on_page_size_and_number(self):
+        sheet = Mock()
+        sheet.get.return_value = [
+            ["James Cameron", "Titanic", 1998, "TRUE"],
+            ["John Lasseter", "Cars", 2006, "FALSE"],
+        ]
+        connector = GoogleSheetConnector(sheet)
+        connector.get_movies_by_page(2, 2)
+        sheet.get.assert_called_with("A4:D5")
