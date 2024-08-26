3cf2d1a28e070a16750fee81a116d59a8a3d8e21
Santiago Ruiz Mazzocato <santimazzo98@gmail.com>
2024-08-26 11:23:29 -0300

    feat: implemented error handling for border cases of /movies endpoint

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,11 @@
 from flask import Flask, jsonify, request
-from google_sheet_connector import GoogleSheetConnector
+from google_sheet_connector import (
+    GoogleSheetConnector,
+    InvalidPageNumberError,
+    InvalidPageSizeError,
+    PageOutOfBoundsError,
+)
+from datetime import datetime
 from sheets import update_movie, add_movie
 from flask_cors import CORS
 import config
@@ -23,9 +29,23 @@ def get_movies():
         page_size = request.args.get("page_size")
         movies = GoogleSheetConnector(sheet).get_movies_by_page(page_number, page_size)
         return jsonify(movies)
-    # Handle different exceptions in an elegant way!
-    except Exception as err:
-        return str(err), 500
+    except Exception as error:
+        match error:
+            case InvalidPageNumberError() | InvalidPageSizeError():
+                status_code = 400
+                status_code_title = "Bad Request"
+            case PageOutOfBoundsError():
+                status_code = 404
+                status_code_title = "Not Found"
+
+        response = {
+            "timestamp": str(datetime.now()),
+            "status": status_code,
+            "error": status_code_title,
+            "message": str(error),
+            "path": request.path,
+        }
+        return response, status_code
 
 
 @app.route("/update", methods=["POST"])
