8937b725c6258d2a171563457ef61e1b3eb4c1e5
Santiago Ruiz Mazzocato <santimazzo98@gmail.com>
2024-08-27 19:10:44 -0300

    test: updated calculate method to omit next page link when given page is the last one

--- backend/page_metadata_calculator.py
+++ backend/page_metadata_calculator.py
@@ -3,18 +3,23 @@ import math
 
 class PageMetadataCalculator:
     def calculate(self, page, movie_count, endpoint):
+        url_prefix = endpoint + "?"
+        page_size_param = f"&size={page.size}"
         page_count = math.ceil(movie_count / page.size)
+
         return {
             "page": page.number,
             "size": page.size,
             "page_count": page_count,
             "movie_count": movie_count,
             "links": {
-                "first": f"/movies?page=1&size={page.size}",
-                "previous": f"/movies?page={(page.number - 1)}&size={page.size}"
+                "first": url_prefix + "page=1" + page_size_param,
+                "previous": url_prefix + f"page={page.number - 1}" + page_size_param
                 if page.number > 1
                 else None,
-                "next": f"/movies?page={(page.number + 1)}&size={page.size}",
-                "last": f"/movies?page={page_count}&size={page.size}",
+                "next": url_prefix + f"page={page.number + 1}" + page_size_param
+                if page.number < page_count
+                else None,
+                "last": url_prefix + f"page={page_count}" + page_size_param,
             },
         }
--- backend/tests/test_page_metadata_calculator.py
+++ backend/tests/test_page_metadata_calculator.py
@@ -4,7 +4,7 @@ from movies_page import MoviesPage
 
 class TestPageMetadataCalculator:
     def test_calculate_metadata_for_first_page(self):
-        metadata = PageMetadataCalculator().calculate(MoviesPage(1, 2), 500, "/movie")
+        metadata = PageMetadataCalculator().calculate(MoviesPage(1, 2), 500, "/movies")
         expected_metadata = {
             "page": 1,
             "size": 2,
@@ -20,7 +20,7 @@ class TestPageMetadataCalculator:
         assert expected_metadata == metadata
 
     def test_calculate_metadata_for_page_in_the_middle(self):
-        metadata = PageMetadataCalculator().calculate(MoviesPage(4, 7), 500, "/movie")
+        metadata = PageMetadataCalculator().calculate(MoviesPage(4, 7), 500, "/movies")
         expected_metadata = {
             "page": 4,
             "size": 7,
@@ -31,6 +31,24 @@ class TestPageMetadataCalculator:
                 "previous": "/movies?page=3&size=7",
                 "next": "/movies?page=5&size=7",
                 "last": "/movies?page=72&size=7",
+            },
+        }
+        assert expected_metadata == metadata
+
+    def test_calculate_metadata_for_last_page(self):
+        metadata = PageMetadataCalculator().calculate(
+            MoviesPage(10, 50), 500, "/movies"
+        )
+        expected_metadata = {
+            "page": 10,
+            "size": 50,
+            "page_count": 10,
+            "movie_count": 500,
+            "links": {
+                "first": "/movies?page=1&size=50",
+                "previous": "/movies?page=9&size=50",
+                "next": None,
+                "last": "/movies?page=10&size=50",
             },
         }
         assert expected_metadata == metadata
