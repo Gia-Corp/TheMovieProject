d8db439eae7cf43f526614e30a7df2a7b99a5f27
Santiago Ruiz Mazzocato <santimazzo98@gmail.com>
2024-09-10 22:33:12 -0300

    test: added validation for movie title & director

--- backend/src/domain/movie.py
+++ backend/src/domain/movie.py
@@ -5,7 +5,31 @@ class Movie:
     def __init__(self, title, director, year, watched):
         if year < 0:
             raise NegativeMovieYearError(year)
+        if not title:
+            raise EmptyMovieTitleError
+        if not director:
+            raise EmptyMovieDirectorError
         return
+
+
+class EmptyMovieDirectorError(ApiException):
+    BAD_REQUEST = 400
+
+    def build_message(self, _):
+        return "Movie director cannot be empty"
+
+    def get_status_code(self):
+        return self.BAD_REQUEST
+
+
+class EmptyMovieTitleError(ApiException):
+    BAD_REQUEST = 400
+
+    def build_message(self, _):
+        return "Movie title cannot be empty"
+
+    def get_status_code(self):
+        return self.BAD_REQUEST
 
 
 class NegativeMovieYearError(ApiException):
--- backend/tests/test_movie.py
+++ backend/tests/test_movie.py
@@ -1,4 +1,9 @@
-from domain.movie import Movie, NegativeMovieYearError
+from domain.movie import (
+    Movie,
+    NegativeMovieYearError,
+    EmptyMovieTitleError,
+    EmptyMovieDirectorError,
+)
 from pytest import raises
 
 
@@ -7,3 +12,13 @@ class TestMovie:
         with raises(NegativeMovieYearError) as error:
             Movie("Argo", "Ben Affleck", -1, True)
         assert "Movie year cannot be negative: -1" in str(error)
+
+    def test_movie_title_cannot_be_empty(self):
+        with raises(EmptyMovieTitleError) as error:
+            Movie("", "Ben Affleck", 2012, True)
+        assert "Movie title cannot be empty" in str(error)
+
+    def test_movie_director_cannot_be_empty(self):
+        with raises(EmptyMovieDirectorError) as error:
+            Movie("Argo", "", 2012, True)
+        assert "Movie director cannot be empty" in str(error)
