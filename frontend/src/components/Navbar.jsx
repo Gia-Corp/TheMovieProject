b05e77c2b392d08e991834b9f8222c9ba1e2395e
Santiago Ruiz Mazzocato <santimazzo98@gmail.com>
2026-02-25 16:55:56 -0300

    front(refactor): extraido css de componente Navbar a su propio archivo css + simplificado estilos y html

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,57 +1,19 @@
+import "./Navbar.css";
+
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-      <div className="container-fluid">
-        <a href="/#" className="navbar-brand">
-          Navbar
+    <nav className="navbar">
+      <div className="nav-buttons">
+        <a id="home-button" href="#">
+          The Movie Project
         </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarColor01"
-          aria-controls="navbarColor01"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarColor01">
-          <ul className="navbar-nav me-auto">
-            <li className="nav-item">
-              <a href="/#" className="nav-link active">
-                Home
-                <span className="visually-hidden">(current)</span>
-              </a>
-            </li>
-            <li className="nav-item">
-              <a href="/#" className="nav-link">
-                Features
-              </a>
-            </li>
-            <li className="nav-item">
-              <a href="/#" className="nav-link">
-                Pricing
-              </a>
-            </li>
-            <li className="nav-item">
-              <a href="/#" className="nav-link">
-                About
-              </a>
-            </li>
-          </ul>
-          <form className="d-flex">
-            <input
-              className="form-control me-sm-2"
-              type="search"
-              placeholder="Search"
-            />
-            <button className="btn btn-secondary my-2 my-sm-0" type="submit">
-              Search
-            </button>
-          </form>
-        </div>
       </div>
+      <form className="search-form">
+        <button className="search-button" type="button">
+          Buscar
+        </button>
+        <input placeholder="Una peli..." type="text" />
+      </form>
     </nav>
   );
 }
