9ef625c9195fa58a245d99a8ab1fdb3b047969e6
Santiago Ruiz Mazzocato <santimazzo98@gmail.com>
2026-02-25 16:34:28 -0300

    front(chore): mejorado script para levantar ambos dev containers

--- start_dev_containers.sh
+++ start_dev_containers.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-docker compose -f docker-compose.dev.yml up -d --build
+docker compose -f docker-compose.dev.yml up -d
+docker compose exec -d backend sh ./start_dev.sh
+docker compose exec frontend sh
