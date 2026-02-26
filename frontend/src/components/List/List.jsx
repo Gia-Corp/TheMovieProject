a1921de0bc76ffe63d46dad0b5625920264b872e
Santiago Ruiz Mazzocato <santimazzo98@gmail.com>
2026-02-26 11:03:58 -0300

    front(refactor): created MovieCard component to replace ListItem

--- frontend/src/components/List/List.jsx
+++ frontend/src/components/List/List.jsx
@@ -1,11 +1,13 @@
 import ListItem from "../ListItem";
+import MovieCard from "../MovieCard/MovieCard";
 import "./List.css";
 
 function List({ list }) {
   return (
     <div className="movies-list">
       {list.map((item) => (
-        <ListItem key={item.id} item={item} />
+        // <ListItem key={item.id} item={item} />
+        <MovieCard key={item.id} />
       ))}
     </div>
   );
---
+++ frontend/src/components/MovieCard/MovieCard.jsx
@@ -0,0 +1,11 @@
+import "./MovieCard.css";
+
+function MovieCard() {
+  return (
+    <div className="tarjeta-pelicula">
+      <div className="placeholder-cargando"></div>
+    </div>
+  );
+}
+
+export default MovieCard;
