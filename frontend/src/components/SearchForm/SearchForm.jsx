fc28f5e959fb33d9f8f279454f51efc208c29abe
Santiago Ruiz Mazzocato <santimazzo98@gmail.com>
2026-03-09 14:48:39 -0300

    feat(front): implemented basic version of SearchForm logic

--- frontend/src/components/SearchForm/SearchForm.jsx
+++ frontend/src/components/SearchForm/SearchForm.jsx
@@ -1,12 +1,56 @@
 import "./SearchForm.css";
+import { useState, useEffect, useRef } from "react";
+import { useRepos } from "../../hooks/useRepos";
 
 function SearchForm() {
+  const [inputText, setInputText] = useState("");
+  const timerRef = useRef(null);
+  const { movieRepository } = useRepos();
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    if (inputText === "") return;
+
+    clearTimeout(timerRef.current);
+    timerRef.current = setTimeout(() => {
+      movieRepository
+        .getMovies({
+          page: 1,
+          size: 20,
+          title: inputText,
+        })
+        .then((res) => setMovies(res["movies"]));
+    }, 500);
+  }, [inputText, movieRepository]);
+
+  const handleInputChange = (event) => {
+    const newInputText = event.target.value;
+    setInputText(newInputText);
+    if (newInputText === "") setMovies([]);
+  };
+
   return (
     <form className="search-form">
-      <button className="search-button" type="button">
-        Buscar
-      </button>
-      <input placeholder="Una peli..." type="text" />
+      <input
+        placeholder="Buscar peli..."
+        type="text"
+        value={inputText}
+        onChange={handleInputChange}
+      />
+      {movies.length > 0 ? (
+        <ul className="dropdown-list">
+          {movies.map((movie) => {
+            return (
+              <li
+                className="dropdown-item"
+                key={movie["id"]}
+              >{`${movie["title"]} (${movie["year"]})`}</li>
+            );
+          })}
+        </ul>
+      ) : (
+        ""
+      )}
     </form>
   );
 }
