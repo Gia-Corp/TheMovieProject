da967b9c764eed8919a3dc53671bed69abc82f0d
Santiago Ruiz Mazzocato <santimazzo98@gmail.com>
2026-03-28 12:48:43 -0300

    feat(front): enhance AddMovieForm with movie search functionality and update Modal width

--- frontend/src/components/AddMovieForm/AddMovieForm.jsx
+++ frontend/src/components/AddMovieForm/AddMovieForm.jsx
@@ -1,15 +1,23 @@
 import "./AddMovieForm.css";
-import { useActionState, useEffect } from "react";
+import { useActionState, useEffect, useRef, useState } from "react";
 import { useRepos } from "@/hooks/useRepos";
 
 function AddMovieForm({ onSuccess }) {
-  const { movieRepository } = useRepos();
+  const { movieRepository, externalMovieRepository } = useRepos();
+  const [inputText, setInputText] = useState("");
+  const [movies, setMovies] = useState(null);
+  const [isFocused, setIsFocused] = useState(false);
+  const [selectedMovie, setSelectedMovie] = useState(null);
+  
+  const timerRef = useRef(null);
+  const blurTimeout = useRef(null);
 
   const [state, dispatch, isPending] = useActionState(
     async (prevState, formData) => {
       try {
         const newMovie = {
-          title: formData.get("title"),
+          title: selectedMovie.Title,
+          year: selectedMovie.Year,
           watched: formData.get("watched") ? true : false,
         };
         await movieRepository.createMovie(newMovie);
@@ -25,19 +33,71 @@ function AddMovieForm({ onSuccess }) {
     if (state.successCount > 0) onSuccess();
   }, [state.successCount, onSuccess]);
 
+  useEffect(() => {
+    if (inputText === "") return;
+
+    clearTimeout(timerRef.current);
+    timerRef.current = setTimeout(() => {
+      externalMovieRepository
+        .getMovies({
+          title: inputText,
+        })
+        .then((res) => setMovies(res.Search))
+        .catch(() => setMovies([]));
+    }, 500);
+  }, [inputText, externalMovieRepository]);
+
+  const handleInputChange = (event) => {
+    const newInputText = event.target.value;
+    setInputText(newInputText);
+    if (newInputText === "") setMovies(null);
+  };
+
+  const handleOnFocus = () => {
+    clearTimeout(blurTimeout.current);
+    setIsFocused(true);
+  };
+  const handleOnBlur = () => {
+    blurTimeout.current = setTimeout(() => setIsFocused(false), 150);
+  };
+  const handleSelect = (movie) => {
+    setSelectedMovie(movie);
+    setInputText(movie.Title);
+    setIsFocused(false);
+  };
+
   return (
     <form key={state.successCount} className="add-movie-form" action={dispatch}>
       <label htmlFor="title">
         <p>Título</p>
         <input
-          type="text"
-          maxLength="100"
+          type="search"
+          maxLength="200"
           name="title"
           id="title"
           placeholder="Apocalypse Now"
+          value={inputText}
+          onChange={handleInputChange}
+          onFocus={handleOnFocus}
+          onBlur={handleOnBlur}
           required
         />
       </label>
+
+      {
+        isFocused && movies ? 
+        <ul id="movie-results">
+          {movies.length > 0 ? movies.map((movie) => 
+            <li 
+              onMouseDown={() => handleSelect(movie)} 
+              key={movie.imdbID}
+            >
+              {movie.Title} ({movie.Year})<img src={movie.Poster}/>
+            </li>
+          ) : <li>No se encontraron pelis</li> } 
+        </ul> : 
+        null
+      }
 
       <label htmlFor="watched">
         <p>Ya la vimos</p>
