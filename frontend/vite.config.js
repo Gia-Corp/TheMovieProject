73f92bc0fd3173f9d93b30ea87fdc9d0474caa56
Santiago Ruiz Mazzocato <santimazzo98@gmail.com>
2026-04-10 11:51:43 -0300

    fix(front): started to use backend proxy url with vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,31 +1,46 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import viteCompression from 'vite-plugin-compression'
 
-export default defineConfig({
-  plugins: [
-    react(),
-    viteCompression({
-      verbose: true,
-      disable: false,
-      threshold: 10240,
-      algorithm: 'gzip',
-      ext: '.gz',
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@': '/src',
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  console.log("BACKEND URL:", env.VITE_BACKEND_URL);
+
+  return {
+    plugins: [
+      react(),
+      viteCompression({
+        verbose: true,
+        disable: false,
+        threshold: 10240,
+        algorithm: 'gzip',
+        ext: '.gz',
+      }),
+    ],
+    resolve: {
+      alias: {
+        '@': '/src',
+      },
     },
-  },
-  server: {
-    port: 3000,
-    host: true 
-  },
-  build: {
-    outDir: 'build'
-  },
-  test: {
-    passWithNoTests: true,
-  },
+    server: {
+      port: 3000,
+      host: true,
+      proxy: {
+        '/api': {
+          target: env.VITE_BACKEND_URL,
+          changeOrigin: true,
+        },
+        '/auth': {
+          target: env.VITE_BACKEND_URL,
+          changeOrigin: true,
+        }
+      }
+    },
+    build: {
+      outDir: 'build'
+    },
+    test: {
+      passWithNoTests: true,
+    },
+  };
 })
