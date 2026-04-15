599156a5313456b6d9394f72b0357b728793f272
Santiago Ruiz Mazzocato <santimazzo98@gmail.com>
2026-04-15 15:58:37 -0300

    refactor(front): extracted delete movie button to its own component

---
+++ frontend/src/components/DeleteMovieButton/DeleteMovieButton.jsx
@@ -0,0 +1,49 @@
+import Modal from "@/components/Modal/Modal";
+import { useRef } from "react";
+// import { useNavigate } from "react-router-dom";
+
+function DeleteMovieButton({ movieId }) {
+  const formModalRef = useRef(null);
+  // const navigate = useNavigate();
+
+  const handleOpen = () => formModalRef.current.showModal();
+  const handleClose = () => formModalRef.current.close();
+
+  console.log(movieId);
+
+  function handleClick() {
+    const MOVIES_ENDPOINT = "/api/movies";
+    const url = MOVIES_ENDPOINT + `/${movieId}`;
+    console.log(url);
+    // fetch(MOVIES_ENDPOINT, {
+    //   method: "DELETE",
+    //   credentials: "include",
+    // }).then(() => {
+    //   handleClose();
+    //   navigate("/", { state: null });
+    // });
+  }
+
+  return (
+    <>
+      <button
+        onClick={handleOpen}
+        className="icon-text-box utility-box delete-button"
+      >
+        <svg viewBox="0 0 24 24">
+          <path d="M18 6L17.1991 18.0129C17.129 19.065 17.0939 19.5911 16.8667 19.99C16.6666 20.3412 16.3648 20.6235 16.0011 20.7998C15.588 21 15.0607 21 14.0062 21H9.99377C8.93927 21 8.41202 21 7.99889 20.7998C7.63517 20.6235 7.33339 20.3412 7.13332 19.99C6.90607 19.5911 6.871 19.065 6.80086 18.0129L6 6M4 6H20M16 6L15.7294 5.18807C15.4671 4.40125 15.3359 4.00784 15.0927 3.71698C14.8779 3.46013 14.6021 3.26132 14.2905 3.13878C13.9376 3 13.523 3 12.6936 3H11.3064C10.477 3 10.0624 3 9.70951 3.13878C9.39792 3.26132 9.12208 3.46013 8.90729 3.71698C8.66405 4.00784 8.53292 4.40125 8.27064 5.18807L8 6M14 10V17M10 10V17" />
+        </svg>
+        <p>Eliminar</p>
+      </button>
+      <Modal title="¿Eliminar peli?" ref={formModalRef} onClose={handleClose}>
+        <div className="confirm-layout">
+          <button onClick={handleClick} className="negative-button">
+            Sí, eliminar
+          </button>
+        </div>
+      </Modal>
+    </>
+  );
+}
+
+export default DeleteMovieButton;
