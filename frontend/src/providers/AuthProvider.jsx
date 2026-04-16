e58dc7060841919d4188b8e941d76ec895919a87
Santiago Ruiz Mazzocato <santimazzo98@gmail.com>
2026-04-16 16:43:14 -0300

    refactor(front): simplified auth provider component code

--- frontend/src/providers/AuthProvider.jsx
+++ frontend/src/providers/AuthProvider.jsx
@@ -4,36 +4,28 @@ import SpinnerIcon from "@/components/SpinnerIcon/SpinnerIcon";
 
 export function AuthProvider({ children }) {
   const [accessToken, setAccessToken] = useState(null);
-  const [loading, setLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     async function tryRefresh() {
       const REFRESH_ENDPOINT = "/auth/refresh";
 
-      try {
-        const response = await fetch(REFRESH_ENDPOINT, {
-          method: "POST",
-          credentials: "include",
+      fetch(REFRESH_ENDPOINT, { method: "POST", credentials: "include" })
+        .then((response) =>
+          response.json().then((data) => {
+            if (!response.ok) throw new Error(data.message);
+            setAccessToken(data.access_token);
+          }),
+        )
+        .finally(() => {
+          setIsLoading(false);
         });
-
-        const data = await response.json();
-
-        if (!response.ok) {
-          throw new Error(data.message);
-        }
-
-        setAccessToken(data.access_token);
-      } catch {
-        // no hay sesión activa, no pasa nada
-      } finally {
-        setLoading(false);
-      }
     }
 
     tryRefresh();
   }, []);
 
-  if (loading)
+  if (isLoading)
     return (
       <div className="spinner-container">
         <SpinnerIcon />
