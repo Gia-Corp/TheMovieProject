9f094c399a7e8ff7d16780a59574111f0dea0540
Santiago Ruiz Mazzocato <santimazzo98@gmail.com>
2026-04-16 16:07:08 -0300

    refactor(front): removed unused poster repository

--- frontend/src/providers/RepositoriesProvider.jsx
+++ frontend/src/providers/RepositoriesProvider.jsx
@@ -1,16 +1,14 @@
 import { MovieRepository } from "@/repositories/MovieRepository";
 import { ExternalMovieRepository } from "@/repositories/ExternalMovieRepository";
-import { PosterRepository } from "@/repositories/PosterRepository";
 import { RepositoriesContext } from "@/hooks/useRepos";
 
 const movieRepository = new MovieRepository();
-const posterRepository = new PosterRepository();
 const externalMovieRepository = new ExternalMovieRepository();
 
 export function RepositoriesProvider({ children }) {
   return (
     <RepositoriesContext.Provider
-      value={{ movieRepository, posterRepository, externalMovieRepository }}
+      value={{ movieRepository, externalMovieRepository }}
     >
       {children}
     </RepositoriesContext.Provider>
