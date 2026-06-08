3de1b05046be8adbc705fcdb2e576bb7697e631b
Santiago Ruiz Mazzocato <santimazzo98@gmail.com>
2026-06-08 11:20:10 -0300

    refactor(front): add EditMovieButton component and integrate it into MovieDetail

---
+++ frontend/src/components/movie_crud/EditMovieButton/EditMovieButton.jsx
@@ -0,0 +1,12 @@
+function EditMovieButton() {
+  return (
+    <button className="icon-text-box utility-box edit-button">
+      <svg viewBox="0 0 24 24">
+        <path d="m13.5 7.5l3 3M4 20v-3.5L15.293 5.207a1 1 0 0 1 1.414 0l2.086 2.086a1 1 0 0 1 0 1.414L7.5 20H4z" />
+      </svg>
+      <p>Editar</p>
+    </button>
+  );
+}
+
+export default EditMovieButton;
