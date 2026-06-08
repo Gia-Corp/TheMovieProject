4d20b3873be74799a644bb7c09e33d042adfcbcc
Santiago Ruiz Mazzocato <santimazzo98@gmail.com>
2026-06-08 12:24:08 -0300

    feat(front): added basic structure of labels and inputs to edit movie form

--- frontend/src/components/movie_crud/EditMovieForm/EditMovieForm.jsx
+++ frontend/src/components/movie_crud/EditMovieForm/EditMovieForm.jsx
@@ -2,7 +2,7 @@ import SubmitButton from "@/components/SubmitButton/SubmitButton";
 
 function EditMovieForm() {
   return (
-    <form className="modal-form">
+    <form className="modal-form" /*action={dispatch}*/>
       <label htmlFor="title">
         <p>Título</p>
         <input
@@ -10,12 +10,47 @@ function EditMovieForm() {
           maxLength="200"
           name="title"
           id="title"
-          placeholder="Devil Wears Prada"
-          // value={inputText}
-          // onChange={handleInputChange}
-          // onFocus={handleOnFocus}
-          // onBlur={handleOnBlur}
-          required
+          defaultValue="Devil Wears Prada"
+        />
+      </label>
+      <label htmlFor="director">
+        <p>Director/es</p>
+        <input
+          type="text"
+          maxLength="200"
+          name="director"
+          id="director"
+          defaultValue="David Frankel"
+        />
+      </label>
+      <label htmlFor="year">
+        <p>Año</p>
+        <input
+          type="number"
+          min="1"
+          name="year"
+          id="year"
+          defaultValue="2006"
+        />
+      </label>
+      <label htmlFor="runtime">
+        <p>Duración (en minutos)</p>
+        <input
+          type="number"
+          min="1"
+          name="runtime"
+          id="runtime"
+          defaultValue="109"
+        />
+      </label>
+      <label htmlFor="plot">
+        <p>Sinopsis</p>
+        <input
+          type="text"
+          maxLength="500"
+          name="plot"
+          id="plot"
+          defaultValue="Andy, a smart but sensible young journalist, starts working as an assistant to the cynical high fashion magazine editor Miranda Priestly."
         />
       </label>
       <SubmitButton text="Confirmar" /*isLoading={isPending}*/ />
