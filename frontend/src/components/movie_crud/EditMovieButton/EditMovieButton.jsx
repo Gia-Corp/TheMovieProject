71b79c91b36731019856d4e6a38c019f27d5badd
Santiago Ruiz Mazzocato <santimazzo98@gmail.com>
2026-06-08 21:59:41 -0300

    refactor(front): removed redundant form key management state

--- frontend/src/components/movie_crud/EditMovieButton/EditMovieButton.jsx
+++ frontend/src/components/movie_crud/EditMovieButton/EditMovieButton.jsx
@@ -1,16 +1,12 @@
-import { useRef, useState } from "react";
+import { useRef } from "react";
 import Modal from "@/components/modals/Modal/Modal";
 import EditMovieForm from "@/components/movie_crud/EditMovieForm/EditMovieForm";
 
 function EditMovieButton({ movie }) {
   const formModalRef = useRef(null);
-  const [formKey, setFormKey] = useState(0);
 
   const handleOpen = () => formModalRef.current.showModal();
-  const handleClose = () => {
-    formModalRef.current.close();
-    setFormKey((prev) => prev + 1);
-  };
+  const handleClose = () => formModalRef.current.close();
 
   return (
     <>
@@ -25,7 +21,7 @@ function EditMovieButton({ movie }) {
       </button>
 
       <Modal title="Editar peli" ref={formModalRef} onClose={handleClose}>
-        <EditMovieForm key={formKey} movie={movie} onSuccess={handleClose} />
+        <EditMovieForm movie={movie} onSuccess={handleClose} />
       </Modal>
     </>
   );
