64462e66ae155c1f73e68f05c374f482cbbba3aa
Santiago Ruiz Mazzocato <santimazzo98@gmail.com>
2026-06-15 12:54:16 -0300

    feat(front): created WatchersList component

---
+++ frontend/src/components/WatchersList/WatchersList.jsx
@@ -0,0 +1,19 @@
+import "./WatchersList.css";
+
+function WatchersList({ watchers, yourId }) {
+  return (
+    <ul className="watchers-list">
+      {watchers.length > 0 ? <p>Vista por:</p> : <p>Nadie la ha visto</p>}
+      {watchers.map((w) => {
+        return (
+          <li key={w.user.id} className="watchers-list-item">
+            <img src={w.user.profile_pic} alt="" />
+            {w.user.id === yourId ? <span>Yo</span> : <p>{w.user.nickname}</p>}
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
+
+export default WatchersList;
