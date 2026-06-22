35903815282a4ba01014bc25a429bf907ef379b4
Santiago Ruiz Mazzocato <santimazzo98@gmail.com>
2026-06-22 16:40:02 -0300

    style(front): remove header from options menu and update profile button styling

--- frontend/src/components/OptionsButton/OptionsButton.jsx
+++ frontend/src/components/OptionsButton/OptionsButton.jsx
@@ -35,13 +35,15 @@ function OptionsButton({ user }) {
       </button>
       {isFocused && (
         <ul className="options-menu">
-          <header>{user.nickname}</header>
           <li>
             <button
               onClick={() => navigate("/profile", { state: { user } })}
-              className="dropdown-button button-with-icon"
+              className="dropdown-button button-with-icon profile-button"
             >
-              Mi perfil
+              <svg viewBox="0 0 640 640">
+                <path d="M320 312C386.3 312 440 258.3 440 192C440 125.7 386.3 72 320 72C253.7 72 200 125.7 200 192C200 258.3 253.7 312 320 312zM290.3 368C191.8 368 112 447.8 112 546.3C112 562.7 125.3 576 141.7 576L498.3 576C514.7 576 528 562.7 528 546.3C528 447.8 448.2 368 349.7 368L290.3 368z" />
+              </svg>
+              <p>Mi perfil</p>
             </button>
           </li>
           <li>
